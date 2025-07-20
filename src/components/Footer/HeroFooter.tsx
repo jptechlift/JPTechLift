8b20453a6314b5ce54ce2d0b4c00f03477dd8a55
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-20 23:15:50 +0700

    Big Update HomePage

--- src/components/Footer/HeroFooter.tsx
+++ src/components/Footer/HeroFooter.tsx
@@ -1,7 +1,19 @@
-// HeroBanner.tsx
+// HeroFooter.tsx
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import backgroundImage from "../../assets/images/BannerFooter.jpg";
 
 export default function HeroFooter() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <section className="relative w-full h-[500px] overflow-hidden">
       {/* Ảnh nền */}
@@ -11,11 +23,14 @@ export default function HeroFooter() {
         className="w-full h-full object-cover object-center absolute inset-0 z-0"
       />
 
-      {/* Lớp phủ đen mờ (optional) */}
+      {/* Lớp phủ đen mờ */}
       <div className="absolute inset-0 bg-black bg-opacity-30 z-10" />
 
       {/* Nội dung chính */}
-      <div className="relative z-20 flex flex-col justify-center items-center text-center h-full px-4">
+      <div
+        className="relative z-20 flex flex-col justify-center items-center text-center h-full px-4"
+        data-aos="zoom-in-up"
+      >
         <h3 className="font-inter text-white text-[44px] md:text-[64px] font-black tracking-wide text-center drop-shadow-[3px_4px_4px_rgba(0,0,0,2)] leading-tight">
           HÃY ĐỂ CHÚNG TÔI
           <br />
