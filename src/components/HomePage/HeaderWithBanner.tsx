ec44b325ca2bf070f415fdb4a01da4e83525b5aa
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-21 19:32:36 +0700

    FIX: HeroBanner with Title

--- src/components/HomePage/HeaderWithBanner.tsx
+++ src/components/HomePage/HeaderWithBanner.tsx
@@ -22,7 +22,7 @@ export default function HeaderWithBanner({ banner, title }: HeaderWithBanner) {
       {/* TITLE */}
       <h1
         className="font-inter pointer-events-none absolute inset-0 z-30 flex items-center justify-center
-                     px-4 mt-12 pt-10 text-center text-3xl font-extrabold uppercase tracking-wide text-white
+                     px-4 md:mt-16 md:pt-10 text-center text-3xl font-extrabold uppercase tracking-wide text-white
                      md:text-5xl"
       >
         {title}
