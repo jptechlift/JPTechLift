b6541aac2f0fbd5559d480e24d11974335bf6bef
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-23 18:44:57 +0700

    Update: Leaders img

--- src/data/LeaderData.ts
+++ src/data/LeaderData.ts
@@ -1,5 +1,6 @@
 // src/data/leaders.ts
 import ThomasEaton from "../assets/images/leaders/ThomasEaton.png";
+import VuNgocMinhTuyet from "../../src/assets/images/leaders/VuNgocMinhTuyet.png";
 
 export interface Leader {
   name: string;
@@ -15,12 +16,12 @@ export const leaders: Leader[] = [
     title: "President JP TechLift UK",
     image: ThomasEaton,
     description:
-      "Định hình tương lai ngành thang máy tại Anh bằng cách tích hợp công nghệ hiện đại vào thiết kế và vận hành.",
+      "Cố vấn công nghệ tại Anh quốc.",
   },
   {
-    name: "Do Hoang Gia Bao",
+    name: "Vũ Ngọc Minh Tuyết",
     title: "President JP TechLift UK",
-    image: ThomasEaton,
+    image: VuNgocMinhTuyet,
     description:
       "Định hình tương lai ngành thang máy tại Anh bằng cách tích hợp công nghệ hiện đại vào thiết kế và vận hành.",
   },
