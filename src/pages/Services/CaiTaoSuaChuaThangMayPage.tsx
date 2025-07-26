089b808f41b8ec69818bb13d8fc9f73c33e7a65c
danhnguyenthanh260 <danhnguyenthanh260@gmail.com>
2025-07-26 20:46:34 +0700

    Resolve merge conflict + update product page outlay

--- src/pages/Services/CaiTaoSuaChuaThangMayPage.tsx
+++ src/pages/Services/CaiTaoSuaChuaThangMayPage.tsx
@@ -1,29 +1,17 @@
 import HeroFooter from "../../components/Footer/HeroFooter";
 import HeroBanner from "../../components/HomePage/HeroBanner";
 import Footer from "../../components/Footer/Footer";
-<<<<<<< HEAD
-import ElevatorInstallationOperation from "../../components/ServicePage/ElevatorInstallationOperation";
-=======
 import CaiTaoSuaChuaThangMay from "../../components/ServicePage/CaiTaoSuaChuaThangMay";
->>>>>>> bf999dd6c428540cda1922b0135929db97124862
 
 const CaiTaoSuaChuaThangMayPage= () => {
   return (
     <div>
       <HeroBanner />
-<<<<<<< HEAD
-      <ElevatorInstallationOperation/>
-=======
       <CaiTaoSuaChuaThangMay/>
->>>>>>> bf999dd6c428540cda1922b0135929db97124862
         <HeroFooter />
       <Footer />
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default CaiTaoSuaChuaThangMayPage;
-=======
-export default CaiTaoSuaChuaThangMayPage;
->>>>>>> bf999dd6c428540cda1922b0135929db97124862
