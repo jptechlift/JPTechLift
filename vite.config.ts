1515e303cc63cbac0f39eec7c18a61b4109d0532
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-27 17:46:15 +0700

    Test1

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
+import { productSlugMap } from './src/constants/productSlugMap';
+
 
 // https://vite.dev/config/
+const staticRoutes = [
+  '/',
+  '/gioi-thieu/quy-trinh-du-an',
+  '/gioi-thieu/nguyen-tac',
+  '/gioi-thieu/ban-lanh-dao',
+  '/tin-tuc-thang-may',
+  '/gioi-thieu',
+  '/blog-thang-may',
+  '/lien-he',
+  '/dich-vu-thang-may',
+  '/dich-vu-thang-may/lap-dat-thang-may',
+  '/dich-vu-thang-may/tu-van-thiet-ke',
+  '/dich-vu-thang-may/huong-dan-van-hanh',
+  '/dich-vu-thang-may/bao-tri-thang-may',
+  '/dich-vu-thang-may/cai-tao-sua-chua',
+];
+
+const productRoutes = Object.values(productSlugMap).map(
+  (slug) => `/san-pham/${slug}`,
+);
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: "https://thangmaysaigonjptechlift.com",
+      hostname: 'https://thangmaysaigonjptechlift.com',
+      routes: [...staticRoutes, ...productRoutes],
     }),
   ],
 
   server: {
     host: true,
   },
-  base: "/",
+  base: '/',
 });
