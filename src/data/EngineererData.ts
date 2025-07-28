0883a796863890425346ef1b250a0f4f6ef6c1b4
dhgb2k5 <dhgb2k5@gmail.com>
2025-07-28 21:16:35 +0700

    Add: LeaderPage

--- src/data/EngineererData.ts
+++ src/data/EngineererData.ts
@@ -1,5 +1,5 @@
 import MinhKhoi from "../../src/assets/images/leaders/MinhKhoi.png";
-
+import Tan from "../../src/assets/images/leaders/Tan.png"
 export interface Engineer {
   name: string;
   title: string;
@@ -15,5 +15,12 @@ export const engineerers: Engineer[] = [
     image: MinhKhoi,
     description:
       "Kiểm tra định kỳ, khắc phục sự cố điện, hỗ trợ lắp đặt và cải tiến hệ thống điều khiển thang máy",
+  },
+  {
+    name: "Tân",
+    title: "Elevator Mechanical Installer",
+    image: Tan,
+    description:
+      "Thực hiện lắp đặt các cấu kiện cơ khí theo bản vẽ kỹ thuật, tiêu chuẩn an toàn và tiến độ thi công.",
   },
 ];
