032f0d7cb25364f3df5e76a51080884b14f2e814
dhgb2k5 <dhgb2k5@gmail.com>
2025-08-13 21:56:28 +0700

    Undo

---
+++ frontend/src/pages/CreateBlogPage.tsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/Navbar/Navbar";
+import CreateBlogForm from "../components/CreateBlogForm";
+import Footer from "../components/Footer/Footer";
+import { BASE_URL } from "../components/SEO/constant";
+import { Helmet } from 'react-helmet-async'; 
+
+const CreateBlogPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Viết Blog - JP TechLift</title>
+        <meta name="description" content="Nơi tạo các Blog về JP TechLift" />
+        <link rel="canonical" href={`${BASE_URL}/tao-blog`} />
+      </Helmet>
+      <Navbar />
+      <CreateBlogForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default CreateBlogPage;
